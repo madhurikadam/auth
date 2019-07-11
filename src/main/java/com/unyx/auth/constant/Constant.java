4461c7600805fc8882724edb229f96526e08f3a7
mk250107 <mk250107@teradata.com>
2019-07-11 20:34:47 +0530

    removed facebook authentication

--- src/main/java/com/unyx/auth/constant/Constant.java
+++ src/main/java/com/unyx/auth/constant/Constant.java
@@ -7,6 +7,6 @@ public class Constant {
     public static final String FB_EXCHANGE_TOKEN = "fb_exchange_token";
     public static final String CLIENT_SECRET = "client_secret";
     public static final String GRANT_TYPE = "grant_type";
-    public static final String FB_CLIENT="624669067712289";
-    public static final String FB_SECRET="9ed2f2614b459e9c30d31e25e7f60d3d";
+    public static final String FB_CLIENT="";
+    public static final String FB_SECRET="";
 }
